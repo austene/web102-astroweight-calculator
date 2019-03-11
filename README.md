e774ba69257fef999f6176802fde3409477e160f
AE <austence@sbcglobal.net>
2019-03-11 09:50:31 -0700

    adds README, commits launch.json, added index.html header information, working calculator code for main.js, and commits package-lock.json.

---
+++ README.md
@@ -0,0 +1,17 @@
+# web102-astroweight-calculator
+
+## Purpose of Program
+
+Calculates weight on different planets in the solar system.
+
+## Setup
+
+```
+$ npm i
+```
+
+## Run
+
+```
+$ npm start 
+```
--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 // Write your JavaScript code here!
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+];
+
+var planetsElement = document.getElementById ("planets");
+
+planets.forEach(function ([planetName, factor]) {
+    var planet = document.createElement ("option");
+    planet.text = planetName;
+    planet.value = planetName;
+    planetsElement.appendChild (planet);
+});
+
+function calculateWeight (weight, planetName) {
+    planets.forEach (function (planet) {
+        if (planetName == planet [0]) {
+            factor = planet [1];
+        }
+    })
+    return weight*factor;
+};
+
+function handleClickEvent (e) {
+    var userWeight = document.getElementById ("user-weight").value;
+    var planetName = document.getElementById ("planets").value;
+    var result = calculateWeight (userWeight, planetName);
+    document.getElementById ("output").innerText = 
+        "If you were on " + planetName + ", you would weigh " + result + "lbs!";
+};
